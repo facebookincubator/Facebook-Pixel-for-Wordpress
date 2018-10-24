f3e7d7f7a03aac8850282a4a75a3deb0293fa37f
Jinzhou Yang <jinzhou@fb.com>
2018-10-24 15:31:58 -0700

    Adding translation files to pixel plugin for wordpress
    Summary: This is to check in translated files into wordpress plugin
    
    Differential Revision: D10842925
    
    fbshipit-source-id: b29e9589

--- core/FacebookPluginConfig.php
+++ core/FacebookPluginConfig.php
@@ -8,7 +8,7 @@ namespace FacebookPixelPlugin\Core;
 defined('ABSPATH') or die('Direct access not allowed');
 
 class FacebookPluginConfig {
-  const PLUGIN_VERSION = '1.7.13';
+  const PLUGIN_VERSION = '1.7.14';
   const SOURCE = 'wordpress';
   const TEXT_DOMAIN = 'official-facebook-pixel';
 
