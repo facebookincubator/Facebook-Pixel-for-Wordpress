2b3d77b176e7ac766073355004d971300f27532b
Jinzhou Yang <jinzhou@fb.com>
2018-12-07 13:46:30 -0800

    Add unit test to make sure the entry point file name cannot be changed
    Summary: as title
    
    Reviewed By: jwlsinx
    
    Differential Revision: D13381744
    
    fbshipit-source-id: 4515395a

---
+++ tests/FileNameTest.php
@@ -0,0 +1,29 @@
+<?php
+/*
+* Copyright (C) 2017-present, Facebook, Inc.
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; version 2 of the License.
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*/
+
+namespace FacebookPixelPlugin\Tests;
+
+use FacebookPixelPlugin\Core\FacebookPixel;
+
+/**
+ * This is a testcase to make sure that the name of entry point file
+ * 'facebook-for-wordpress.php' cannot be changed. Changing the file
+ * name will break how WordPress find the plugin file and will end
+ * up deactivate the plugin.
+**/
+final class FileNameTest extends FacebookWordpressTestBase {
+  public function testEntryPointFileNamePersists() {
+    $exist = \file_exists(dirname(__FILE__).'/../facebook-for-wordpress.php');
+    $this->assertTrue($exist);
+  }
+}
