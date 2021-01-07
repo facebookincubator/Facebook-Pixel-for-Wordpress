180fa557ba226ed93c4bc0c379cbe1081faf511f
José Antonio Jiménez <josejia@fb.com>
2021-01-07 11:06:02 -0800

    Using JSON functions instead of serialization to send events async
    Summary:
    Instead of using serialization functions in the class ServerEventAsyncTask, json functions are used to send events asynchronously. The method normalize from the Event class is used to convert an event to JSON string in prepare_data; and the event is rebuilt in the run_action method, using classes from the business sdk. Because the keys in the normalized data are different from the keys used in the constructors, they are parsed in run_action.
    
    This was done because using php serialization functions  is unsafe to use with user requests (https://www.php.net/manual/en/function.unserialize.php) as demonstrated by the white hat researcher in the associated task.
    
    Reviewed By: vinothsa
    
    Differential Revision: D25787153
    
    fbshipit-source-id: 95572cf59ab866ceea02c38a350215fe2f80c570

--- core/ServerEventAsyncTask.php
+++ core/ServerEventAsyncTask.php
@@ -19,16 +19,102 @@ namespace FacebookPixelPlugin\Core;
 
 use FacebookPixelPlugin\Core\FacebookServerSideEvent;
 
+use FacebookAds\Object\ServerSide\Event;
+use FacebookAds\Object\ServerSide\UserData;
+use FacebookAds\Object\ServerSide\CustomData;
+use FacebookAds\Object\ServerSide\Content;
+
 defined('ABSPATH') or die('Direct access not allowed');
 
 class ServerEventAsyncTask extends \WP_Async_Task {
   protected $action = 'send_server_events';
 
+  private function convert_user_data($user_data_normalized){
+    $norm_key_to_key = [
+      AAMSettingsFields::EMAIL => 'email',
+      AAMSettingsFields::FIRST_NAME => 'first_name',
+      AAMSettingsFields::LAST_NAME => 'last_name',
+      AAMSettingsFields::GENDER => 'gender',
+      AAMSettingsFields::DATE_OF_BIRTH => 'date_of_birth',
+      AAMSettingsFields::EXTERNAL_ID => 'external_id',
+      AAMSettingsFields::PHONE => 'phone',
+      AAMSettingsFields::CITY => 'city',
+      AAMSettingsFields::STATE => 'state',
+      AAMSettingsFields::ZIP_CODE => 'zip_code',
+      AAMSettingsFields::COUNTRY => 'country_code',
+    ];
+    $user_data = array();
+    foreach($user_data_normalized as $norm_key => $field){
+      if(array_key_exists($norm_key, $norm_key_to_key)){
+        $user_data[$norm_key_to_key[$norm_key]] = $field;
+      }
+      else{
+        $user_data[$norm_key] = $field;
+      }
+    }
+    return $user_data;
+  }
+
+  private function convert_array_to_event($event_as_array){
+    $event = new Event($event_as_array);
+    // If user_data exists, an UserData object is created
+    // and set
+    if(array_key_exists('user_data', $event_as_array)){
+      // The method convert_user_data converts the keys used in the
+      // normalized array to the keys used in the constructor of UserData
+      $user_data = new UserData($this->convert_user_data(
+        $event_as_array['user_data']
+      ));
+      $event->setUserData($user_data);
+    }
+    // If custom_data exists, a CustomData object is created and set
+    if(array_key_exists('custom_data', $event_as_array)){
+      $custom_data = new CustomData($event_as_array['custom_data']);
+      // If contents exists in custom_data, an array of Content is created
+      // and set
+      if(array_key_exists('contents', $event_as_array['custom_data'])){
+        $contents = array();
+        foreach(
+          $event_as_array['custom_data']['contents'] as $contents_as_array
+        ){
+          // The normalized contents array encodes product id as id
+          // but the constructor of Content requires product_id
+          if(array_key_exists('id', $contents_as_array)){
+            $contents_as_array['product_id'] = $contents_as_array['id'];
+          }
+          $contents[] = new Content($contents_as_array);
+        }
+        $custom_data->setContents($contents);
+      }
+      if(array_key_exists('fb_integration_tracking',
+        $event_as_array['custom_data'])){
+        $custom_data->addCustomProperty('fb_integration_tracking',
+          $event_as_array['custom_data']['fb_integration_tracking']);
+      }
+      $event->setCustomData($custom_data);
+    }
+    return $event;
+  }
+
   protected function prepare_data($data) {
     try {
       if (!empty($data)) {
+        $num_events = $data[1];
+        $events = $data[0];
+        // $data[0] can be a single event or an array
+        // We want to receive it as an array
+        if($num_events == 1){
+          $events = array($events);
+        }
+        // Each event is casted to a php array with normalize()
+        $events_as_array = array();
+        foreach($events as $event){
+          $events_as_array[] = $event->normalize();
+        }
+        // The array of events is converted to a JSON string
+        // and encoded in base 64
         return array(
-          'event_data' => base64_encode(serialize($data[0])),
+          'event_data' => base64_encode(json_encode($events_as_array)),
           'num_events'=>$data[1]
         );
       }
@@ -45,14 +131,19 @@ class ServerEventAsyncTask extends \WP_Async_Task {
       if( $num_events == 0 ){
         return;
       }
-      $events = unserialize(base64_decode($_POST['event_data']));
-      // When an array has just one object, the deserialization process
-      // returns just the object
-      // and we want an array
-      if( $num_events == 1 ){
-        $events = array( $events );
+      // $_POST['event_data'] is decoded from base 64, returning a JSON string
+      // and decoded as a php array
+      $events_as_array = json_decode(base64_decode($_POST['event_data']), true);
+      // If the passed json string is invalid, no processing is done
+      if(!$events_as_array){
+        return;
       }
-
+      $events = array();
+      // Every event is a php array and casted to an Event object
+      foreach( $events_as_array as $event_as_array ){
+        $event = $this->convert_array_to_event($event_as_array);
+        $events[] = $event;
+      }
       FacebookServerSideEvent::send($events);
     }
     catch (\Exception $ex) {
