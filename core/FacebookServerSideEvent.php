c9d2dcd72d861e1f3bbbdd30391891d3d9e8ec87
Freddy Bisaso <bisaso@meta.com>
2023-04-21 05:36:40 -0700

    Fix SDK 16 compatibility issue with PHP7
    Summary: PHP 7 compatibility issue
    
    Reviewed By: vahidkay-meta
    
    Differential Revision: D45180989
    
    fbshipit-source-id: 1649c039ed89c9c997d6e4d00eecf4a7cf351ff3

--- core/FacebookServerSideEvent.php
+++ core/FacebookServerSideEvent.php
@@ -88,10 +88,7 @@ class FacebookServerSideEvent {
     $access_token = FacebookWordpressOptions::getAccessToken();
     $agent = FacebookWordpressOptions::getAgentString();
 
-    // If events are from openbridge, add _capi in user agent
-    if (count($events) === 1 &&
-    (($events[0]['custom_data']['custom_properties']['fb_integration_tracking']
-        ?? NULL) === 'wp-cloudbridge-plugin')) {
+    if(self::isOpenBridgeEvent($events)){
       $agent .= '_capi';
     }
 
@@ -109,5 +106,23 @@ class FacebookServerSideEvent {
     } catch (Exception $e) {
       error_log(json_encode($e));
     }
+  }
+
+  private static function isOpenBridgeEvent($events) {
+    if(count($events) !== 1){
+        return false;
+    }
+
+    $customData = $events[0]->getCustomData();
+    if (!$customData) {
+        return false;
+    }
+
+    $customProperties = $customData->getCustomProperties();
+    if (!$customProperties || !isset($customProperties['fb_integration_tracking'])) {
+        return false;
+    }
+
+    return $customProperties['fb_integration_tracking'] === 'wp-cloudbridge-plugin';
   }
 }
