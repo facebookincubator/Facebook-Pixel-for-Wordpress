6df19ff7744cdd15462773b6c18e4883f7a28bfb
Nikola Pavlovic <nikola@stuntcoders.com>
2024-10-24 16:36:45 +0200

    Fix codestyle for FacebookWordpressGetVersionNumberTask

--- task/FacebookWordpressGetVersionNumberTask.php
+++ task/FacebookWordpressGetVersionNumberTask.php
@@ -1,32 +1,49 @@
-<?php
-/*
-* Copyright (C) 2017-present, Meta, Inc.
-*
-* This program is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation; version 2 of the License.
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*/
+<?php //phpcs:ignore WordPress.Files.FileName.NotHyphenatedLowercase WordPress.Files.FileName.InvalidClassFileName
+/**
+ * Facebook Pixel Plugin FacebookWordpressGetVersionNumberTask class.
+ *
+ * This file contains the main logic for FacebookWordpressGetVersionNumberTask.
+ *
+ * @package FacebookPixelPlugin
+ */
+
+/**
+ * Define FacebookWordpressGetVersionNumberTask class.
+ *
+ * @return void
+ */
 
 use FacebookPixelPlugin\Core\FacebookPluginConfig;
 
+/**
+ * FacebookWordpressGetVersionNumberTask class.
+ */
 class FacebookWordpressGetVersionNumberTask extends BaseTask {
 
 	/**
-	 * The version number property
+	 * Versionprop variable.
+	 *
+	 * @var [string]
 	 */
 	private $versionprop;
 
+
 	/**
-	 * The setter for the attribute "versionprop"
+	 * Set the property in the project that should contain the version number of the plugin.
+	 *
+	 * @param string $versionprop The name of the property in the project that should contain the version number.
+	 *
+	 * @return void
 	 */
 	public function setVersionProp( $versionprop ) {
 		$this->versionprop = $versionprop;
 	}
 
+	/**
+	 * Sets the project property specified in $versionprop to the current version of the plugin.
+	 *
+	 * @return void
+	 */
 	public function main() {
 		$version = FacebookPluginConfig::PLUGIN_VERSION;
 		$this->project->setProperty( $this->versionprop, $version );
