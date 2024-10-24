ea9911670e8c455809db5c052d3d7063f099ceb1
Nikola Pavlovic <nikola@stuntcoders.com>
2024-10-24 16:43:48 +0200

    Update code style for EDDUtils and fix function names after renaming

--- integration/EDDUtils.php
+++ integration/EDDUtils.php
@@ -1,30 +1,44 @@
-<?php
-/*
- * Copyright (C) 2017-present, Meta, Inc.
+<?php //phpcs:ignore WordPress.Files.FileName.NotHyphenatedLowercase WordPress.Files.FileName.InvalidClassFileName
+/**
+ * Facebook Pixel Plugin EDDUtils class.
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; version 2 of the License.
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * This file contains the main logic for EDDUtils.
+ *
+ * @package FacebookPixelPlugin
  */
 
 /**
- * @package FacebookPixelPlugin
+ * Define EDDUtils class.
+ *
+ * @return void
  */
 
 namespace FacebookPixelPlugin\Integration;
 
-defined( 'ABSPATH' ) or die( 'Direct access not allowed' );
+defined( 'ABSPATH' ) || die( 'Direct access not allowed' );
 
+/**
+ * EDDUtils class.
+ */
 class EDDUtils {
-	public static function getCurrency() {
+	/**
+	 * Return the currency code.
+	 *
+	 * @since 3.0.0
+	 * @return string The currency code.
+	 */
+	public static function get_currency() {
 		return edd_get_currency();
 	}
 
-	public static function getCartTotal() {
+	/**
+	 * Get the total of the cart.
+	 *
+	 * @since 3.0.0
+	 *
+	 * @return float The total amount of the cart.
+	 */
+	public static function get_cart_total() {
 		return EDD()->cart->get_total();
 	}
 }
