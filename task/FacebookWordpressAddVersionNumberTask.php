2aec67ce7ff552e324c22c10407ea0971f1bb7af
Nikola Pavlovic <nikola@stuntcoders.com>
2024-10-24 16:35:09 +0200

    Update codestyle for FacebookWordpressAddVersionNumberTask class

--- task/FacebookWordpressAddVersionNumberTask.php
+++ task/FacebookWordpressAddVersionNumberTask.php
@@ -1,38 +1,58 @@
-<?php
-/*
-* Copyright (C) 2017-present, Meta, Inc.
-*
-* This program is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation; version 2 of the License.
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*/
+<?php //phpcs:ignore WordPress.Files.FileName.NotHyphenatedLowercase WordPress.Files.FileName.InvalidClassFileName
+/**
+ * Facebook Pixel Plugin FacebookWordpressAddVersionNumberTask class.
+ *
+ * This file contains the main logic for FacebookWordpressAddVersionNumberTask.
+ *
+ * @package FacebookPixelPlugin
+ */
 
-require_once \dirname( __FILE__ ) . '/BaseTask.php';
+/**
+ * Define FacebookWordpressAddVersionNumberTask class.
+ *
+ * @return void
+ */
+
+require_once __DIR__ . '/BaseTask.php';
 
 use FacebookPixelPlugin\Core\FacebookPluginConfig;
 
+/**
+ * FacebookWordpressAddVersionNumberTask class.
+ */
 class FacebookWordpressAddVersionNumberTask extends BaseTask {
 
 	/**
-	 * The path of file we want to add version number to
+	 * Path variable.
+	 *
+	 * @var [string]
 	 */
 	private $path = null;
 
 	/**
-	 * The setter for the attribute "path"
+	 * Setter for the path property.
+	 *
+	 * @param string $path The path to the file to update with the plugin version.
+	 *
+	 * @return void
 	 */
 	public function setPath( $path ) {
 		$this->path = $path;
 	}
 
+	/**
+	 * Main function to update the version number in the specified file.
+	 *
+	 * This function retrieves the current plugin version and replaces the
+	 * placeholder '{*VERSION_NUMBER*}' in the file at the specified path
+	 * with the actual version number.
+	 *
+	 * @return void
+	 */
 	public function main() {
 		$this->setABSPATH();
 		$version = FacebookPluginConfig::PLUGIN_VERSION;
-		echo "The current version is {$version} \n";
+		echo 'The current version is ' . $version . " \n";
 		$file_contents = file_get_contents( $this->path );
 		$file_contents = str_replace( '{*VERSION_NUMBER*}', $version, $file_contents );
 		file_put_contents( $this->path, $file_contents );
