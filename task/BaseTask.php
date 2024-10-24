e5fbbd6e697d072a89437d9250fae291d8312f6c
Nikola Pavlovic <nikola@stuntcoders.com>
2024-10-24 16:23:12 +0200

    Fix task baseTask code style

--- task/BaseTask.php
+++ task/BaseTask.php
@@ -1,26 +1,41 @@
-<?php
-/*
-* Copyright (C) 2017-present, Meta, Inc.
-*
-* This program is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation; version 2 of the License.
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*/
+<?php //phpcs:ignore WordPress.Files.FileName.NotHyphenatedLowercase WordPress.Files.FileName.InvalidClassFileName
+/**
+ * Facebook Pixel Plugin BaseTask class.
+ *
+ * This file contains the main logic for BaseTask.
+ *
+ * @package FacebookPixelPlugin
+ */
 
-require_once dirname( __FILE__ ) . '/../vendor/phing/phing/src/Phing/Task.php';
-require_once dirname( __FILE__ ) . '/../vendor/autoload.php';
+/**
+ * Define BaseTask class.
+ *
+ * @return void
+ */
+
+require_once __DIR__ . '/../vendor/phing/phing/src/Phing/Task.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 use Phing\Task;
 
+/**
+ * BaseTask class.
+ */
 abstract class BaseTask extends Task {
 
+	/**
+	 * Sets the ABSPATH constant if it is not already defined.
+	 *
+	 * WordPress needs this constant to be set in order to load. We can't
+	 * assume that the constant is set in the context of our tasks, since
+	 * they are being run from the command line instead of through a
+	 * web request.
+	 *
+	 * @return void
+	 */
 	public function setABSPATH() {
 		if ( ! defined( 'ABSPATH' ) ) {
-			define( 'ABSPATH', dirname( __FILE__ ) . '/../' );
+			define( 'ABSPATH', __DIR__ . '/../' );
 		}
 	}
 }
