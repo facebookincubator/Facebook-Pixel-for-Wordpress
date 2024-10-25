e3306db66b5dc38ecef7b7da9492973e181c2389
Nikola Pavlovic <nikola@stuntcoders.com>
2024-10-25 11:15:58 +0200

    Update code style for FacebookWordpressNinjaForms

--- integration/FacebookWordpressNinjaForms.php
+++ integration/FacebookWordpressNinjaForms.php
@@ -1,23 +1,33 @@
-<?php
-/*
- * Copyright (C) 2017-present, Meta, Inc.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; version 2 of the License.
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- */
-
+<?php //phpcs:ignore WordPress.Files.FileName.NotHyphenatedLowercase WordPress.Files.FileName.InvalidClassFileName
 /**
+ * Facebook Pixel Plugin FacebookWordpressNinjaForms class.
+ *
+ * This file contains the main logic for FacebookWordpressNinjaForms.
+ *
  * @package FacebookPixelPlugin
  */
 
+/**
+ * Define FacebookWordpressNinjaForms class.
+ *
+ * @return void
+ */
+
+/*
+* Copyright (C) 2017-present, Meta, Inc.
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; version 2 of the License.
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*/
+
 namespace FacebookPixelPlugin\Integration;
 
-defined( 'ABSPATH' ) or die( 'Direct access not allowed' );
+defined( 'ABSPATH' ) || die( 'Direct access not allowed' );
 
 use FacebookPixelPlugin\Core\FacebookPixel;
 use FacebookPixelPlugin\Core\FacebookPluginUtils;
@@ -28,10 +38,23 @@ use FacebookPixelPlugin\Core\PixelRenderer;
 use FacebookAds\Object\ServerSide\Event;
 use FacebookAds\Object\ServerSide\UserData;
 
+/**
+ * FacebookWordpressNinjaForms class.
+ */
 class FacebookWordpressNinjaForms extends FacebookWordpressIntegrationBase {
 	const PLUGIN_FILE   = 'ninja-forms/ninja-forms.php';
 	const TRACKING_NAME = 'ninja-forms';
 
+	/**
+	 * Injects Facebook Pixel code for Ninja Forms.
+	 *
+	 * This method hooks into the 'ninja_forms_submission_actions' action,
+	 * which is triggered during the form submission process in Ninja Forms.
+	 * It adds the 'injectLeadEvent' method to handle form submission events,
+	 * allowing the tracking of lead events with Facebook Pixel.
+	 *
+	 * @return void
+	 */
 	public static function injectPixelCode() {
 		add_action(
 			'ninja_forms_submission_actions',
@@ -41,6 +64,22 @@ class FacebookWordpressNinjaForms extends FacebookWordpressIntegrationBase {
 		);
 	}
 
+	/**
+	 * Injects lead event code into the form submission process of Ninja Forms.
+	 *
+	 * This method hooks into the 'ninja_forms_submission_actions' action, which is
+	 * triggered during the form submission process in Ninja Forms. It injects code
+	 * into the form submission action, which is triggered when the form is submitted
+	 * successfully. The code generates a lead event for the form submission and renders
+	 * it as pixel code using the PixelRenderer. The resulting code is appended to the
+	 * success message of the form submission action.
+	 *
+	 * @param array $actions An array of form submission actions.
+	 * @param array $form_cache An array of form cache data.
+	 * @param array $form_data An array of form data.
+	 *
+	 * @return array An array of form submission actions with the injected code.
+	 */
 	public static function injectLeadEvent( $actions, $form_cache, $form_data ) {
 		if ( FacebookPluginUtils::isInternalUser() ) {
 			return $actions;
@@ -56,8 +95,7 @@ class FacebookWordpressNinjaForms extends FacebookWordpressIntegrationBase {
 				continue;
 			}
 
-			// inject code when form is submitted successfully
-			if ( $type == 'successmessage' ) {
+			if ( 'successmessage' === $type ) {
 				$event = ServerEventFactory::safeCreateEvent(
 					'Lead',
 					array( __CLASS__, 'readFormData' ),
@@ -85,6 +123,24 @@ class FacebookWordpressNinjaForms extends FacebookWordpressIntegrationBase {
 		return $actions;
 	}
 
+	/**
+	 * Reads form data from the $_POST global array.
+	 *
+	 * This function extracts user-related data such as email, first name, last name,
+	 * phone number, and address details from the $_POST array, commonly used in form
+	 * submissions. The extracted data includes:
+	 * - 'email': The user's email address.
+	 * - 'first_name': The user's first name.
+	 * - 'last_name': The user's last name.
+	 * - 'phone': The user's phone number.
+	 * - 'city', 'state', 'zip', 'country': Address details, where the country must
+	 *   be specified using a 2-letter code.
+	 *
+	 * The function returns an associative array containing the extracted data.
+	 *
+	 * @param array $form_data The form data as an associative array.
+	 * @return array An associative array of form data.
+	 */
 	public static function readFormData( $form_data ) {
 		if ( empty( $form_data ) ) {
 			return array();
@@ -110,10 +166,24 @@ class FacebookWordpressNinjaForms extends FacebookWordpressIntegrationBase {
 		return $event_data;
 	}
 
+	/**
+	 * Retrieves the user's email address from the form data.
+	 *
+	 * @param array $form_data The form data as an associative array.
+	 * @return string The user's email address.
+	 */
 	private static function getEmail( $form_data ) {
 		return self::getField( $form_data, 'email' );
 	}
 
+	/**
+	 * Retrieves the user's full name from the form data and splits it into
+	 * first name and last name.
+	 *
+	 * @param array $form_data The form data as an associative array.
+	 * @return array|null An array containing first name and last name, or null
+	 *                    if no name field is found.
+	 */
 	private static function getName( $form_data ) {
 		$name = self::getField( $form_data, 'name' );
 		if ( $name ) {
@@ -122,43 +192,108 @@ class FacebookWordpressNinjaForms extends FacebookWordpressIntegrationBase {
 		return null;
 	}
 
+	/**
+	 * Retrieves the user's first name from the form data.
+	 *
+	 * @param array $form_data The form data as an associative array.
+	 * @return string The user's first name.
+	 */
 	private static function getFirstName( $form_data ) {
 		return self::getField( $form_data, 'firstname' );
 	}
 
+	/**
+	 * Retrieves the user's last name from the form data.
+	 *
+	 * @param array $form_data The form data as an associative array.
+	 * @return string The user's last name.
+	 */
 	private static function getLastName( $form_data ) {
 		return self::getField( $form_data, 'lastname' );
 	}
 
+	/**
+	 * Retrieves the user's phone number from the form data.
+	 *
+	 * @param array $form_data The form data as an associative array.
+	 * @return string The user's phone number.
+	 */
 	private static function getPhone( $form_data ) {
 		return self::getField( $form_data, 'phone' );
 	}
 
+	/**
+	 * Retrieves the user's city from the form data.
+	 *
+	 * @param array $form_data The form data as an associative array.
+	 * @return string The user's city.
+	 */
 	private static function getCity( $form_data ) {
 		return self::getField( $form_data, 'city' );
 	}
 
+	/**
+	 * Retrieves the user's zip code from the form data.
+	 *
+	 * @param array $form_data The form data as an associative array.
+	 * @return string The user's zip code.
+	 */
 	private static function getZipCode( $form_data ) {
 		return self::getField( $form_data, 'zip' );
 	}
 
+	/**
+	 * Retrieves the user's state from the form data.
+	 *
+	 * @param array $form_data The form data as an associative array.
+	 * @return string The user's state.
+	 */
 	private static function getState( $form_data ) {
 		return self::getField( $form_data, 'liststate' );
 	}
 
+	/**
+	 * Retrieves the user's country from the form data.
+	 *
+	 * @param array $form_data The form data as an associative array.
+	 * @return string The user's country.
+	 */
 	private static function getCountry( $form_data ) {
 		return self::getField( $form_data, 'listcountry' );
 	}
 
+	/**
+	 * Retrieves the user's gender from the form data.
+	 *
+	 * @param array $form_data The form data as an associative array.
+	 * @return string The user's gender.
+	 */
 	private static function getGender( $form_data ) {
 		return self::getField( $form_data, 'gender' );
 	}
 
-	private static function hasPrefix( $string, $prefix ) {
+	/**
+	 * Checks if a given string starts with a given prefix.
+	 *
+	 * @param string $text The string to check.
+	 * @param string $prefix The prefix to check for.
+	 * @return boolean True if the string starts with the prefix, false otherwise.
+	 */
+	private static function hasPrefix( $text, $prefix ) {
 		$len = strlen( $prefix );
-		return substr( $string, 0, $len ) === $prefix;
+		return substr( $text, 0, $len ) === $prefix;
 	}
 
+	/**
+	 * Retrieves the value of a field from the form data by its key.
+	 *
+	 * The key is searched for in the form data as a prefix of the field's key.
+	 * If a matching field is found, its value is returned. If not, null is returned.
+	 *
+	 * @param array  $form_data The form data as an associative array.
+	 * @param string $key The key of the field to retrieve the value for.
+	 * @return string|null The value of the field, or null if no matching field is found.
+	 */
 	private static function getField( $form_data, $key ) {
 		if ( empty( $form_data['fields'] ) ) {
 			return null;
