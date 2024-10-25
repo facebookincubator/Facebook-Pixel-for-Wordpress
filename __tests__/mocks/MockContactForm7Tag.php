0c64a12adc4bf58ec63ec8cddbe76c3f27f8342f
Nikola Pavlovic <nikola@stuntcoders.com>
2024-10-25 13:58:31 +0200

    Update code style for MockContactForm7Tag

--- __tests__/mocks/MockContactForm7Tag.php
+++ __tests__/mocks/MockContactForm7Tag.php
@@ -1,23 +1,56 @@
-<?php
-/*
- * Copyright (C) 2017-present, Meta, Inc.
+<?php //phpcs:ignore WordPress.Files.FileName.NotHyphenatedLowercase WordPress.Files.FileName.InvalidClassFileName
+/**
+ * Facebook Pixel Plugin MockContactForm7Tag class.
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; version 2 of the License.
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * This file contains the main logic for MockContactForm7Tag.
+ *
+ * @package FacebookPixelPlugin
  */
+
+/**
+ * Define MockContactForm7Tag class.
+ *
+ * @return void
+ */
+
+/*
+* Copyright (C) 2017-present, Meta, Inc.
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; version 2 of the License.
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*/
 
 namespace FacebookPixelPlugin\Tests\Mocks;
 
+/**
+ * MockContactForm7Tag class.
+ */
 final class MockContactForm7Tag {
+	/**
+	 * The base type of the contact form 7 tag.
+	 *
+	 * @var string
+	 */
 	public $basetype;
+		/**
+		 * The name of the contact form 7 tag.
+		 *
+		 * @var string
+		 */
 	public $name;
 
-	function __construct( $basetype, $name ) {
+	/**
+	 * Initializes the MockContactForm7Tag object.
+	 *
+	 * @param string $basetype The base type of the contact form 7 tag.
+	 * @param string $name     The name of the contact form 7 tag.
+	 */
+	public function __construct( $basetype, $name ) {
 		$this->basetype = $basetype;
 		$this->name     = $name;
 	}
