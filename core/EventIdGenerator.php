ed7b14f64fe9bd0bc3bd3bfe470468cb2f740571
Nikola Pavlovic <nikola@stuntcoders.com>
2024-10-25 16:44:10 +0200

    Fix code style for EventIdGenerator

--- core/EventIdGenerator.php
+++ core/EventIdGenerator.php
@@ -1,22 +1,29 @@
-<?php
+<?php //phpcs:ignore WordPress.Files.FileName.NotHyphenatedLowercase WordPress.Files.FileName.InvalidClassFileName
 /**
- * Copyright (C) 2015-present, Meta, Inc.
+ * Facebook Pixel Plugin EventIdGenerator class.
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; version 2 of the License.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * This file contains the main logic for EventIdGenerator.
  *
  * @package FacebookPixelPlugin
  */
+
+/**
+ * Define EventIdGenerator class.
+ *
+ * @return void
+ */
+
+/*
+* Copyright (C) 2017-present, Meta, Inc.
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; version 2 of the License.
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*/
 
 namespace FacebookPixelPlugin\Core;
 
@@ -32,8 +39,8 @@ final class EventIdGenerator {
 	public static function guidv4() {
 		$data = openssl_random_pseudo_bytes( 16 );
 
-		$data[6] = chr( ord( $data[6] ) & 0x0f | 0x40 ); // set version to 0100
-		$data[8] = chr( ord( $data[8] ) & 0x3f | 0x80 ); // set bits 6-7 to 10
+		$data[6] = chr( ord( $data[6] ) & 0x0f | 0x40 );
+		$data[8] = chr( ord( $data[8] ) & 0x3f | 0x80 );
 
 		return vsprintf( '%s%s-%s-%s-%s-%s%s%s', str_split( bin2hex( $data ), 4 ) );
 	}
