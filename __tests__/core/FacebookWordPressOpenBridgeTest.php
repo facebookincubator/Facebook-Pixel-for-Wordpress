2bc53fda34a365fe727354e53366f9a1703d0e96
Nikola Pavlovic <nikola@stuntcoders.com>
2024-10-25 16:21:20 +0200

    Update code style for FacebookWordPressOpenBridgeTest

--- __tests__/core/FacebookWordPressOpenBridgeTest.php
+++ __tests__/core/FacebookWordPressOpenBridgeTest.php
@@ -1,15 +1,29 @@
-<?php
-/*
- * Copyright (C) 2017-present, Meta, Inc.
+<?php //phpcs:ignore WordPress.Files.FileName.NotHyphenatedLowercase WordPress.Files.FileName.InvalidClassFileName
+/**
+ * Facebook Pixel Plugin FacebookWordPressOpenBridgeTest class.
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; version 2 of the License.
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * This file contains the main logic for FacebookWordPressOpenBridgeTest.
+ *
+ * @package FacebookPixelPlugin
  */
+
+/**
+ * Define FacebookWordPressOpenBridgeTest class.
+ *
+ * @return void
+ */
+
+/*
+* Copyright (C) 2017-present, Meta, Inc.
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; version 2 of the License.
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*/
 
 namespace FacebookPixelPlugin\Tests\Core;
 
@@ -19,16 +33,18 @@ use FacebookPixelPlugin\Core\FacebookWordpressOpenBridge;
 use FacebookPixelPlugin\Tests\FacebookWordpressTestBase;
 
 /**
- * @runTestsInSeparateProcesses
- * @preserveGlobalState disabled
- *
- * All tests in this test class should be run in separate PHP process to
- * make sure tests are isolated.
- * Stop preserving global state from the parent process.
+ * FacebookWordPressOpenBridgeTest class.
  */
 final class FacebookWordPressOpenBridgeTest extends FacebookWordpressTestBase {
 
-
+	/**
+	 * Test that when no logged in user is present, a new GUID is generated.
+	 *
+	 * This test verifies that the FacebookWordpressOpenBridge class correctly
+	 * generates a new GUID and stores it in the session when no logged in
+	 * user is present. It also verifies that the GUID is properly returned
+	 * in the external_id field of the server event.
+	 */
 	public function testWhenNoLoggedInUserNewGUIDShouldBeGenerated() {
 		self::mockFacebookWordpressOptions();
 		\WP_Mock::userFunction( 'wp_get_current_user', array( 'return' => array() ) );
@@ -36,14 +52,25 @@ final class FacebookWordPressOpenBridgeTest extends FacebookWordpressTestBase {
 		\WP_Mock::userFunction( 'com_create_guid', array( 'return' => 'GUID' ) );
 		$_SESSION['obeid'] = 'GUID';
 
-		$event              = ServerEventFactory::newEvent( 'Lead' );
-		$openBridgeInstance = FacebookWordpressOpenBridge::get_instance();
+		$event                = ServerEventFactory::newEvent( 'Lead' );
+		$open_bridge_instance = FacebookWordpressOpenBridge::get_instance();
 
-		$ev = $openBridgeInstance->extract_from_databag( $event );
+		$ev = $open_bridge_instance->extract_from_databag( $event );
 
 		$this->assertEquals( array( 'GUID' ), $ev['external_id'] );
 	}
 
+	/**
+	 * Sets up mock user and billing address data for testing.
+	 *
+	 * This method uses WP_Mock to simulate the return values for various
+	 * WordPress functions related to the current user and their billing
+	 * address. It mocks `wp_get_current_user`, `get_current_user_id`,
+	 * and `get_user_meta` calls to provide predefined user information
+	 * and billing address data for testing purposes.
+	 *
+	 * @return void
+	 */
 	private function setupCustomerBillingAddress() {
 		\WP_Mock::userFunction(
 			'wp_get_current_user',
@@ -122,47 +149,70 @@ final class FacebookWordPressOpenBridgeTest extends FacebookWordpressTestBase {
 		);
 	}
 
+	/**
+	 * Test that external_id is fetched from the cookie when obeid exists in the databag.
+	 *
+	 * @covers FacebookWordpressOpenBridge::extract_from_databag
+	 */
 	public function testExternalIdFetchedFromCookieWhenObeidExists() {
 		self::mockFacebookWordpressOptions();
 		\WP_Mock::userFunction( 'wp_get_current_user', array( 'return' => array() ) );
 		\WP_Mock::userFunction( 'get_current_user_id', array( 'return' => 0 ) );
 		$_SESSION['obeid'] = 'testObeid';
 
-		$event              = ServerEventFactory::newEvent( 'Lead' );
-		$openBridgeInstance = FacebookWordpressOpenBridge::get_instance();
+		$event                = ServerEventFactory::newEvent( 'Lead' );
+		$open_bridge_instance = FacebookWordpressOpenBridge::get_instance();
 
-		$ev = $openBridgeInstance->extract_from_databag( $event );
+		$ev = $open_bridge_instance->extract_from_databag( $event );
 
 		$this->assertEquals( array( 'testObeid' ), $ev['external_id'] );
 	}
 
+	/**
+	 * Test that external_id is fetched from the user ID if it's the first time.
+	 *
+	 * This test ensures that when the user ID is available,
+	 * it is included in the external_id array along with the obeid from the session.
+	 *
+	 * @covers FacebookWordpressOpenBridge::extract_from_databag
+	 */
 	public function testExternalIdFetchedFromUserIdIfFirstTime() {
 		self::mockFacebookWordpressOptions();
 		$this->setupCustomerBillingAddress();
 		$_SESSION['obeid'] = 'testObeid';
 
-		$event              = ServerEventFactory::newEvent( 'Lead' );
-		$openBridgeInstance = FacebookWordpressOpenBridge::get_instance();
+		$event                = ServerEventFactory::newEvent( 'Lead' );
+		$open_bridge_instance = FacebookWordpressOpenBridge::get_instance();
 
-		$ev = $openBridgeInstance->extract_from_databag( $event );
+		$ev = $open_bridge_instance->extract_from_databag( $event );
 
 		$this->assertEquals( array( '1', 'testObeid' ), $ev['external_id'] );
 	}
 
+	/**
+	 * Test that external_id is fetched from AAM if it's the first time
+	 * and the user ID is not found.
+	 *
+	 * This test ensures that when the user ID is unavailable,
+	 * the external_id is sourced from the advanced matching settings (AAM)
+	 * and combined with the obeid from the session.
+	 *
+	 * @covers FacebookWordpressOpenBridge::extract_from_databag
+	 */
 	public function testExternalIdFetchedFromAAMIfFirstTimeAndUserIdNotFound() {
 		self::mockFacebookWordpressOptions();
 		\WP_Mock::userFunction( 'wp_get_current_user', array( 'return' => array() ) );
 		\WP_Mock::userFunction( 'get_current_user_id', array( 'return' => 0 ) );
 		$_SESSION['obeid'] = 'testObeid';
 
-		$openBridgeInstance = FacebookWordpressOpenBridge::get_instance();
-		$event              = array(
+		$open_bridge_instance = FacebookWordpressOpenBridge::get_instance();
+		$event                = array(
 			'fb.advanced_matching' => array(
 				'external_id' => 'testAM',
 			),
 		);
 
-		$ev = $openBridgeInstance->extract_from_databag( $event );
+		$ev = $open_bridge_instance->extract_from_databag( $event );
 
 		$this->assertEquals( array( 'testAM', 'testObeid' ), $ev['external_id'] );
 	}
