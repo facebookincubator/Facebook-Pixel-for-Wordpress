776ffb3acbdb21464f456e293c455801a775718f
Nikola Pavlovic <nikola@stuntcoders.com>
2024-10-25 13:49:26 +0200

    Update code style for MockContactForm7

--- __tests__/mocks/MockContactForm7.php
+++ __tests__/mocks/MockContactForm7.php
@@ -1,26 +1,65 @@
-<?php
-/*
- * Copyright (C) 2017-present, Meta, Inc.
+<?php //phpcs:ignore WordPress.Files.FileName.NotHyphenatedLowercase WordPress.Files.FileName.InvalidClassFileName
+/**
+ * Facebook Pixel Plugin MockContactForm7 class.
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; version 2 of the License.
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * This file contains the main logic for MockContactForm7.
+ *
+ * @package FacebookPixelPlugin
  */
+
+/**
+ * Define MockContactForm7 class.
+ *
+ * @return void
+ */
+
+/*
+* Copyright (C) 2017-present, Meta, Inc.
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; version 2 of the License.
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*/
 
 namespace FacebookPixelPlugin\Tests\Mocks;
 
+/**
+ * MockContactForm7 class.
+ */
 final class MockContactForm7 {
+	/**
+	 * An array of form tags.
+	 *
+	 * @var array
+	 */
 	private $form_tags = array();
-	private $throw     = false;
+	/**
+	 * Whether to throw an exception when get_current() is called.
+	 *
+	 * @var bool
+	 */
+	private $throw = false;
 
-	public function set_throw( $throw ) {
-		$this->throw = $throw;
+	/**
+	 * Sets whether to throw an exception when get_current() is called.
+	 *
+	 * @param bool $status Whether to throw an exception.
+	 */
+	public function set_throw( $status ) {
+		$this->throw = $status;
 	}
 
+	/**
+	 * Adds a form tag to the mock plugin.
+	 *
+	 * @param string $basetype The base type of the tag.
+	 * @param string $name     The name of the tag.
+	 * @param mixed  $value    The value of the tag.
+	 */
 	public function add_tag( $basetype, $name, $value ) {
 		$tag            = new MockContactForm7Tag( $basetype, $name );
 		$_POST[ $name ] = $value;
@@ -28,6 +67,15 @@ final class MockContactForm7 {
 		$this->form_tags[] = $tag;
 	}
 
+	/**
+	 * Scans and retrieves the form tags.
+	 *
+	 * If the 'throw' property is set to true, an exception is thrown.
+	 *
+	 * @throws \Exception If an error occurs during form tag scanning.
+	 *
+	 * @return array The array of form tags.
+	 */
 	public function scan_form_tags() {
 		if ( $this->throw ) {
 			throw new \Exception( 'Error scanning form tags!' );
