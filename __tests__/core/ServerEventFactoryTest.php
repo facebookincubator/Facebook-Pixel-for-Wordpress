f6026548a2ebaba3a7c3e3a3b7b1154172aeb08f
Nikola Pavlovic <nikola@stuntcoders.com>
2024-10-25 16:38:38 +0200

    Fix code style for ServerEventFactoryTest

--- __tests__/core/ServerEventFactoryTest.php
+++ __tests__/core/ServerEventFactoryTest.php
@@ -1,15 +1,29 @@
-<?php
-/*
- * Copyright (C) 2017-present, Meta, Inc.
+<?php //phpcs:ignore WordPress.Files.FileName.NotHyphenatedLowercase WordPress.Files.FileName.InvalidClassFileName
+/**
+ * Facebook Pixel Plugin ServerEventFactoryTest class.
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; version 2 of the License.
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * This file contains the main logic for ServerEventFactoryTest.
+ *
+ * @package FacebookPixelPlugin
  */
+
+/**
+ * Define ServerEventFactoryTest class.
+ *
+ * @return void
+ */
+
+/*
+* Copyright (C) 2017-present, Meta, Inc.
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; version 2 of the License.
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*/
 
 namespace FacebookPixelPlugin\Tests\Core;
 
@@ -20,14 +34,18 @@ use FacebookPixelPlugin\Tests\FacebookWordpressTestBase;
 use FacebookAds\Object\ServerSide\AdsPixelSettings;
 
 /**
- * @runTestsInSeparateProcesses
- * @preserveGlobalState disabled
- *
- * All tests in this test class should be run in separate PHP process to
- * make sure tests are isolated.
- * Stop preserving global state from the parent process.
+ * ServerEventFactoryTest class.
  */
 final class ServerEventFactoryTest extends FacebookWordpressTestBase {
+	/**
+	 * Tests that a new event has a valid event ID.
+	 *
+	 * This test verifies that the event ID generated for a new event
+	 * is not null and has a length of 36 characters, which corresponds
+	 * to the expected format of a UUID.
+	 *
+	 * @return void
+	 */
 	public function testNewEventHasEventId() {
 		$event = ServerEventFactory::newEvent( 'Lead' );
 
@@ -35,6 +53,15 @@ final class ServerEventFactoryTest extends FacebookWordpressTestBase {
 		$this->assertEquals( 36, strlen( $event->getEventId() ) );
 	}
 
+	/**
+	 * Tests that a new event has a valid event time.
+	 *
+	 * This test verifies that the event time generated for a new event
+	 * is not null and is a valid Unix timestamp, i.e. it is less than
+	 * the current time.
+	 *
+	 * @return void
+	 */
 	public function testNewEventHasEventTime() {
 		$event = ServerEventFactory::newEvent( 'Lead' );
 
@@ -42,17 +69,42 @@ final class ServerEventFactoryTest extends FacebookWordpressTestBase {
 		$this->assertLessThan( 1, time() - $event->getEventTime() );
 	}
 
+	/**
+	 * Tests that a new event has the correct event name.
+	 *
+	 * This test verifies that the event name generated for a new event
+	 * matches the event name passed to the newEvent method.
+	 *
+	 * @return void
+	 */
 	public function testNewEventHasEventName() {
 		$event = ServerEventFactory::newEvent( 'Lead' );
 
 		$this->assertEquals( 'Lead', $event->getEventName() );
 	}
 
+	/**
+	 * Tests that a new event has an action source.
+	 *
+	 * This test verifies that the action source generated for a new event
+	 * is 'website'.
+	 *
+	 * @return void
+	 */
 	public function testNewEventHasActionSource() {
 		$event = ServerEventFactory::newEvent( 'ViewContent' );
 		$this->assertEquals( 'website', $event->getActionSource() );
 	}
 
+	/**
+	 * Tests that a new event can be created with an IPv4 address.
+	 *
+	 * This test verifies that when an IPv4 address is passed in the
+	 * X-Forwarded-For header, a new event can be created with the correct
+	 * client IP address.
+	 *
+	 * @return void
+	 */
 	public function testNewEventWorksWithIpV4() {
 		$_SERVER['HTTP_X_FORWARDED_FOR'] = '24.17.77.101';
 
@@ -63,6 +115,15 @@ final class ServerEventFactoryTest extends FacebookWordpressTestBase {
 		);
 	}
 
+	/**
+	 * Tests that a new event can be created with an IPv6 address.
+	 *
+	 * This test verifies that when an IPv6 address is passed in the
+	 * X-Forwarded-For header, a new event can be created with the correct
+	 * client IP address.
+	 *
+	 * @return void
+	 */
 	public function testNewEventWorksWithIpV6() {
 		$_SERVER['HTTP_X_FORWARDED_FOR'] = '2120:10a:c191:401::5:7170';
 
@@ -73,6 +134,15 @@ final class ServerEventFactoryTest extends FacebookWordpressTestBase {
 		);
 	}
 
+	/**
+	 * Tests that a new event takes the first IP address from a list.
+	 *
+	 * This test verifies that when multiple IP addresses are provided
+	 * in the X-Forwarded-For header, the first IP address is correctly
+	 * used as the client IP address for the new event.
+	 *
+	 * @return void
+	 */
 	public function testNewEventTakesFirstWithIpAddressList() {
 		$_SERVER['HTTP_X_FORWARDED_FOR']
 		= '2120:10a:c191:401::5:7170, 24.17.77.101';
@@ -84,6 +154,15 @@ final class ServerEventFactoryTest extends FacebookWordpressTestBase {
 		);
 	}
 
+	/**
+	 * Tests that a new event honors the precedence order for determining the client's IP address.
+	 *
+	 * This test verifies that when both the HTTP_X_FORWARDED_FOR and REMOTE_ADDR
+	 * headers are present, the HTTP_X_FORWARDED_FOR header is used to determine
+	 * the client's IP address for the new event.
+	 *
+	 * @return void
+	 */
 	public function testNewEventHonorsPrecedenceForIpAddress() {
 		$_SERVER['HTTP_X_FORWARDED_FOR'] = '24.17.77.101';
 		$_SERVER['REMOTE_ADDR']          = '24.17.77.100';
@@ -95,6 +174,16 @@ final class ServerEventFactoryTest extends FacebookWordpressTestBase {
 		);
 	}
 
+	/**
+	 * Tests that a new event handles an invalid IP address correctly.
+	 *
+	 * This test verifies that when an invalid IP address is provided
+	 * in the HTTP_X_FORWARDED_FOR header, the client's IP address for
+	 * the new event is null, indicating that the invalid IP address was
+	 * not used.
+	 *
+	 * @return void
+	 */
 	public function testNewEventWithInvalidIpAddress() {
 		$_SERVER['HTTP_X_FORWARDED_FOR'] = 'INVALID';
 
@@ -102,6 +191,14 @@ final class ServerEventFactoryTest extends FacebookWordpressTestBase {
 		$this->assertNull( $event->getUserData()->getClientIpAddress() );
 	}
 
+	/**
+	 * Tests that a new event has the correct user agent.
+	 *
+	 * This test verifies that the user agent generated for a new event
+	 * matches the user agent value set in the HTTP_USER_AGENT server variable.
+	 *
+	 * @return void
+	 */
 	public function testNewEventHasUserAgent() {
 		$_SERVER['HTTP_USER_AGENT'] = 'HTTP_USER_AGENT_VALUE';
 		$event                      = ServerEventFactory::newEvent( 'Lead' );
@@ -112,6 +209,14 @@ final class ServerEventFactoryTest extends FacebookWordpressTestBase {
 		);
 	}
 
+	/**
+	 * Tests that a new event has the correct event source URL with HTTPS.
+	 *
+	 * This test verifies that when the HTTPS server variable is set,
+	 * the event source URL generated for a new event includes the 'https' scheme.
+	 *
+	 * @return void
+	 */
 	public function testNewEventHasEventSourceUrlWithHttps() {
 		$_SERVER['HTTPS']       = 'anyvalue';
 		$_SERVER['HTTP_HOST']   = 'www.pikachu.com';
@@ -122,6 +227,14 @@ final class ServerEventFactoryTest extends FacebookWordpressTestBase {
 		$this->assertEquals( 'https://www.pikachu.com/index.php', $event->getEventSourceUrl() );
 	}
 
+	/**
+	 * Tests that a new event has the correct event source URL with HTTP.
+	 *
+	 * This test verifies that when the HTTPS server variable is not set,
+	 * the event source URL generated for a new event includes the 'http' scheme.
+	 *
+	 * @return void
+	 */
 	public function testNewEventHasEventSourceUrlWithHttp() {
 		$_SERVER['HTTPS']       = '';
 		$_SERVER['HTTP_HOST']   = 'www.pikachu.com';
@@ -132,6 +245,14 @@ final class ServerEventFactoryTest extends FacebookWordpressTestBase {
 		$this->assertEquals( 'http://www.pikachu.com/index.php', $event->getEventSourceUrl() );
 	}
 
+	/**
+	 * Tests that a new event has the correct event source URL with HTTP when HTTPS is set to 'off'.
+	 *
+	 * This test verifies that when the HTTPS server variable is explicitly set to 'off',
+	 * the event source URL generated for a new event includes the 'http' scheme.
+	 *
+	 * @return void
+	 */
 	public function testNewEventHasEventSourceUrlWithHttpsOff() {
 		$_SERVER['HTTPS']       = 'off';
 		$_SERVER['HTTP_HOST']   = 'www.pikachu.com';
@@ -142,6 +263,16 @@ final class ServerEventFactoryTest extends FacebookWordpressTestBase {
 		$this->assertEquals( 'http://www.pikachu.com/index.php', $event->getEventSourceUrl() );
 	}
 
+	/**
+	 * Tests that a new event has the correct event source URL when the prefer_referer_for_event_src flag is set to true.
+	 *
+	 * This test verifies that when the HTTP_REFERER server variable is set
+	 * and the prefer_referer_for_event_src flag is set to true,
+	 * the event source URL generated for a new event is the value in the
+	 * HTTP_REFERER server variable.
+	 *
+	 * @return void
+	 */
 	public function testNewEventEventSourceUrlPreferReferer() {
 		$_SERVER['HTTPS']        = 'off';
 		$_SERVER['HTTP_HOST']    = 'www.pikachu.com';
@@ -153,6 +284,17 @@ final class ServerEventFactoryTest extends FacebookWordpressTestBase {
 		$this->assertEquals( 'http://referrer/', $event->getEventSourceUrl() );
 	}
 
+	/**
+	 * Tests that a new event has the correct event source URL when the prefer_referer_for_event_src flag is set to true,
+	 * but the HTTP_REFERER server variable is not set.
+	 *
+	 * This test verifies that when the HTTP_REFERER server variable is not set
+	 * and the prefer_referer_for_event_src flag is set to true,
+	 * the event source URL generated for a new event is the value in the
+	 * HTTP_HOST and REQUEST_URI server variables.
+	 *
+	 * @return void
+	 */
 	public function testNewEventEventSourceUrlWithoutReferer() {
 		$_SERVER['HTTPS']       = 'off';
 		$_SERVER['HTTP_HOST']   = 'www.pikachu.com';
@@ -163,6 +305,14 @@ final class ServerEventFactoryTest extends FacebookWordpressTestBase {
 		$this->assertEquals( 'http://www.pikachu.com/index.php', $event->getEventSourceUrl() );
 	}
 
+	/**
+	 * Tests that the fbclid is extracted from the URL if the FBC cookie is not found.
+	 *
+	 * This test verifies that when the FBC cookie is not set, the fbclid parameter
+	 * from the URL is used to set the FBC value in the event.
+	 *
+	 * @return void
+	 */
 	public function testFBClidExtractedFromUrlIfFbcNotFound() {
 		$_GET['fbclid'] = 'fbclid_str';
 
@@ -174,6 +324,14 @@ final class ServerEventFactoryTest extends FacebookWordpressTestBase {
 		$this->assertEquals( true, str_ends_with( $event_fbc, '.fbclid_str' ) );
 	}
 
+	/**
+	 * Tests that a new event correctly uses the FBC value from the cookie.
+	 *
+	 * This test verifies that when the FBC cookie is set, the FBC value
+	 * is correctly retrieved and assigned to the event's user data.
+	 *
+	 * @return void
+	 */
 	public function testNewEventHasFbc() {
 		$_COOKIE['_fbc'] = '_fbc_value';
 		$event           = ServerEventFactory::newEvent( 'Lead' );
@@ -181,6 +339,14 @@ final class ServerEventFactoryTest extends FacebookWordpressTestBase {
 		$this->assertEquals( '_fbc_value', $event->getUserData()->getFbc() );
 	}
 
+	/**
+	 * Tests that a new event correctly uses the FBP value from the cookie.
+	 *
+	 * This test verifies that when the FBP cookie is set, the FBP value
+	 * is correctly retrieved and assigned to the event's user data.
+	 *
+	 * @return void
+	 */
 	public function testNewEventHasFbp() {
 		$_COOKIE['_fbp'] = '_fbp_value';
 		$event           = ServerEventFactory::newEvent( 'Lead' );
@@ -188,6 +354,16 @@ final class ServerEventFactoryTest extends FacebookWordpressTestBase {
 		$this->assertEquals( '_fbp_value', $event->getUserData()->getFbp() );
 	}
 
+	/**
+	 * Tests the safeCreateEvent method with Personally Identifiable Information (PII).
+	 *
+	 * This test verifies that when PII extraction is enabled and all AAM fields
+	 * are available, the safeCreateEvent method correctly creates a server event
+	 * with the expected user data attributes, including email, phone, first name,
+	 * last name, state, city, country code, zip code, and gender.
+	 *
+	 * @return void
+	 */
 	public function testSafeCreateEventWithPII() {
 		$this->mockUseAAM( '1234', true, AAMSettingsFields::get_all_fields() );
 
@@ -211,6 +387,14 @@ final class ServerEventFactoryTest extends FacebookWordpressTestBase {
 		$this->assertEquals( 'm', $server_event->getUserData()->getGender() );
 	}
 
+	/**
+	 * Tests the safeCreateEvent method with Personally Identifiable Information (PII) extraction disabled.
+	 *
+	 * This test verifies that when PII extraction is disabled, the safeCreateEvent method correctly creates a server event
+	 * with all user data attributes set to null.
+	 *
+	 * @return void
+	 */
 	public function testSafeCreateEventWithPIIDisabled() {
 
 		$server_event = ServerEventFactory::safeCreateEvent(
@@ -231,6 +415,15 @@ final class ServerEventFactoryTest extends FacebookWordpressTestBase {
 		$this->assertNull( $server_event->getUserData()->getGender() );
 	}
 
+	/**
+	 * Returns a sample event data array.
+	 *
+	 * This method returns an associative array containing sample user data fields
+	 * such as email, first name, last name, phone, state, city, country, zip code,
+	 * and gender. These fields are used for testing purposes.
+	 *
+	 * @return array The sample event data array with user information.
+	 */
 	public function getEventData() {
 		return array(
 			'email'      => 'pika.chu@s2s.com',
@@ -245,6 +438,22 @@ final class ServerEventFactoryTest extends FacebookWordpressTestBase {
 		);
 	}
 
+	/**
+	 * Mocks the use of AAM settings for tests.
+	 *
+	 * This method sets up a mock of the AdsPixelSettings class and the FacebookWordpressOptions class.
+	 * The AdsPixelSettings class is configured with the given pixel ID, whether AAM is enabled,
+	 * and which fields are enabled for AAM.
+	 *
+	 * The method then sets up the FacebookWordpressOptions class to return the configured AdsPixelSettings
+	 * instance when the get_aam_settings method is called.
+	 *
+	 * @param string $pixel_id The ID of the pixel.
+	 * @param bool   $enable_aam Whether AAM is enabled.
+	 * @param array  $enable_aam_fields The fields to enable for AAM.
+	 *
+	 * @return void
+	 */
 	private function mockUseAAM(
 		$pixel_id = '1234',
 		$enable_aam = false,
