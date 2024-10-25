c3f0d16b650c897ad9f8b0d99a19b59ddbbf4700
Nikola Pavlovic <nikola@stuntcoders.com>
2024-10-25 16:33:49 +0200

    Update code style for FacebookWordpressSettingsRecorderTest

--- __tests__/core/FacebookWordpressSettingsRecorderTest.php
+++ __tests__/core/FacebookWordpressSettingsRecorderTest.php
@@ -1,15 +1,29 @@
-<?php
-/*
- * Copyright (C) 2017-present, Meta, Inc.
+<?php //phpcs:ignore WordPress.Files.FileName.NotHyphenatedLowercase WordPress.Files.FileName.InvalidClassFileName
+/**
+ * Facebook Pixel Plugin FacebookWordpressSettingsRecorderTest class.
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; version 2 of the License.
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * This file contains the main logic for FacebookWordpressSettingsRecorderTest.
+ *
+ * @package FacebookPixelPlugin
  */
+
+/**
+ * Define FacebookWordpressSettingsRecorderTest class.
+ *
+ * @return void
+ */
+
+/*
+* Copyright (C) 2017-present, Meta, Inc.
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; version 2 of the License.
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*/
 
 namespace FacebookPixelPlugin\Tests\Core;
 
@@ -18,27 +32,39 @@ use FacebookPixelPlugin\Tests\FacebookWordpressTestBase;
 use FacebookPixelPlugin\Core\FacebookPluginConfig;
 
 /**
- * @runTestsInSeparateProcesses
- * @preserveGlobalState disabled
- *
- * All tests in this test class should be run in separate PHP process to
- * make sure tests are isolated.
- * Stop preserving global state from the parent process.
+ * FacebookWordpressSettingsRecorderTest class.
  */
 final class FacebookWordpressSettingsRecorderTest extends FacebookWordpressTestBase {
+	/**
+	 * Verifies that the Ajax actions are added to WordPress.
+	 *
+	 * We're verifying that the saveFbeSettings and deleteFbeSettings methods are
+	 * properly added as Ajax actions in WordPress.
+	 *
+	 * @covers FacebookWordpressSettingsRecorder::init
+	 */
 	public function testAjaxActionsAdded() {
-		$settingsRecorder = new FacebookWordpressSettingsRecorder();
+		$settings_recorder = new FacebookWordpressSettingsRecorder();
 		\WP_Mock::expectActionAdded(
 			'wp_ajax_save_fbe_settings',
-			array( $settingsRecorder, 'saveFbeSettings' )
+			array( $settings_recorder, 'saveFbeSettings' )
 		);
 		\WP_Mock::expectActionAdded(
 			'wp_ajax_delete_fbe_settings',
-			array( $settingsRecorder, 'deleteFbeSettings' )
+			array( $settings_recorder, 'deleteFbeSettings' )
 		);
-		$settingsRecorder->init();
+		$settings_recorder->init();
 	}
 
+	/**
+	 * Mocks WordPress functions for tests.
+	 *
+	 * This function sets up stubs for WordPress functions that are used in the
+	 * FacebookWordpressSettingsRecorder class. The stubs are set up to return
+	 * true for all invocations.
+	 *
+	 * @return void
+	 */
 	public function mockWordPressFunctions() {
 		\WP_Mock::userFunction(
 			'current_user_can',
@@ -66,10 +92,20 @@ final class FacebookWordpressSettingsRecorderTest extends FacebookWordpressTestB
 		);
 	}
 
+	/**
+	 * Tests that invalid settings are not saved.
+	 *
+	 * This test verifies that the FacebookWordpressSettingsRecorder class does not save
+	 * settings when the pixel ID, access token, or external business ID contain invalid
+	 * values. The test case sets up invalid values for the $_POST superglobal and
+	 * verifies that the saveFbeSettings method returns an error.
+	 *
+	 * @covers FacebookWordpressSettingsRecorder::saveFbeSettings
+	 */
 	public function testDoesNotSaveInvalidSettings() {
-		$settingsRecorder = new FacebookWordpressSettingsRecorder();
+		$settings_recorder = new FacebookWordpressSettingsRecorder();
 		self::mockWordPressFunctions();
-		global $_POST;
+		global $_POST; //phpcs:ignore WordPress.Security.NonceVerification.Missing
 		$_POST['pixelId']            = '</script><script>alert(1)</script>';
 		$_POST['accessToken']        = '</script><script>alert(2)</script>';
 		$_POST['externalBusinessId'] = '</script><script>alert(3)</script>';
@@ -83,42 +119,55 @@ final class FacebookWordpressSettingsRecorderTest extends FacebookWordpressTestB
 				),
 			)
 		);
-		$expectedJson = array(
+		$expected_json = array(
 			'success' => false,
 			'msg'     => 'Invalid values',
 		);
-		$result       = $settingsRecorder->saveFbeSettings();
-		$this->assertEquals( $expectedJson, $result );
+		$result        = $settings_recorder->saveFbeSettings();
+		$this->assertEquals( $expected_json, $result );
 	}
 
+	/**
+	 * Tests that settings are saved when the current user is an administrator.
+	 *
+	 * This test verifies that the FacebookWordpressSettingsRecorder class saves
+	 * settings when the current user is an administrator. It sets up valid values
+	 * for the $_POST superglobal and calls the saveFbeSettings method. The test
+	 * verifies that the settings are saved correctly by comparing the result
+	 * with the expected output.
+	 *
+	 * @covers FacebookWordpressSettingsRecorder::saveFbeSettings
+	 */
 	public function testSaveSettingsWithAdmin() {
-		$settingsRecorder = new FacebookWordpressSettingsRecorder();
+		$settings_recorder = new FacebookWordpressSettingsRecorder();
 		self::mockWordPressFunctions();
-		global $_POST;
+		global $_POST; //phpcs:ignore WordPress.Security.NonceVerification.Missing
 		$_POST['pixelId']            = '123';
 		$_POST['accessToken']        = 'ABC123XYZ';
 		$_POST['externalBusinessId'] = 'fbe_wordpress_123_abc';
-		\WP_Mock::userFunction(
-			'sanitize_text_field',
-			array(
-				'return_in_order' => array(
-					$_POST['pixelId'],
-					$_POST['accessToken'],
-					$_POST['externalBusinessId'],
+		if ( isset( $_POST['pixelId'] ) && isset( $_POST['accessToken'] ) && isset( $_POST['externalBusinessId'] ) ) { //phpcs:ignore WordPress.Security.NonceVerification.Missing
+			\WP_Mock::userFunction(
+				'sanitize_text_field',
+				array(
+					'return_in_order' => array(
+						sanitize_text_field( wp_unslash( $_POST['pixelId'] ) ), //phpcs:ignore WordPress.Security.NonceVerification.Missing
+						sanitize_text_field( wp_unslash( $_POST['accessToken'] ) ), //phpcs:ignore WordPress.Security.NonceVerification.Missing
+						sanitize_text_field( wp_unslash( $_POST['externalBusinessId'] ) ), //phpcs:ignore WordPress.Security.NonceVerification.Missing
+					),
+				)
+			);
+			$expected_json = array(
+				'success' => true,
+				'msg'     => array(
+					FacebookPluginConfig::PIXEL_ID_KEY     => sanitize_text_field( wp_unslash( $_POST['pixelId'] ) ), //phpcs:ignore WordPress.Security.NonceVerification.Missing
+					FacebookPluginConfig::ACCESS_TOKEN_KEY => sanitize_text_field( wp_unslash( $_POST['accessToken'] ) ), //phpcs:ignore WordPress.Security.NonceVerification.Missing
+					FacebookPluginConfig::EXTERNAL_BUSINESS_ID_KEY =>
+					sanitize_text_field( wp_unslash( $_POST['externalBusinessId'] ) ), //phpcs:ignore WordPress.Security.NonceVerification.Missing
+					FacebookPluginConfig::IS_FBE_INSTALLED_KEY => '1',
 				),
-			)
-		);
-		$expectedJson = array(
-			'success' => true,
-			'msg'     => array(
-				FacebookPluginConfig::PIXEL_ID_KEY         => $_POST['pixelId'],
-				FacebookPluginConfig::ACCESS_TOKEN_KEY     => $_POST['accessToken'],
-				FacebookPluginConfig::EXTERNAL_BUSINESS_ID_KEY =>
-					$_POST['externalBusinessId'],
-				FacebookPluginConfig::IS_FBE_INSTALLED_KEY => '1',
-			),
-		);
-		$result       = $settingsRecorder->saveFbeSettings();
-		$this->assertEquals( $expectedJson, $result );
+			);
+			$result        = $settings_recorder->saveFbeSettings();
+			$this->assertEquals( $expected_json, $result );
+		}
 	}
 }
