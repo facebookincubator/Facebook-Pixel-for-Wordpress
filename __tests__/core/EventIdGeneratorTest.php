0834d5c195c0ffb48e763d21794b1c3936c2ec28
Nikola Pavlovic <nikola@stuntcoders.com>
2024-10-25 16:13:25 +0200

    Update code style for EventIdGeneratorTest

--- __tests__/core/EventIdGeneratorTest.php
+++ __tests__/core/EventIdGeneratorTest.php
@@ -1,15 +1,29 @@
-<?php
-/*
- * Copyright (C) 2017-present, Meta, Inc.
+<?php //phpcs:ignore WordPress.Files.FileName.NotHyphenatedLowercase WordPress.Files.FileName.InvalidClassFileName
+/**
+ * Facebook Pixel Plugin EventIdGeneratorTest class.
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; version 2 of the License.
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * This file contains the main logic for EventIdGeneratorTest.
+ *
+ * @package FacebookPixelPlugin
  */
+
+/**
+ * Define EventIdGeneratorTest class.
+ *
+ * @return void
+ */
+
+/*
+* Copyright (C) 2017-present, Meta, Inc.
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; version 2 of the License.
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*/
 
 namespace FacebookPixelPlugin\Tests\Core;
 
@@ -17,14 +31,17 @@ use FacebookPixelPlugin\Core\EventIdGenerator;
 use FacebookPixelPlugin\Tests\FacebookWordpressTestBase;
 
 /**
- * @runTestsInSeparateProcesses
- * @preserveGlobalState disabled
- *
- * All tests in this test class should be run in separate PHP process to
- * make sure tests are isolated.
- * Stop preserving global state from the parent process.
+ * EventIdGeneratorTest class.
  */
 final class EventIdGeneratorTest extends FacebookWordpressTestBase {
+	/**
+	 * Test that guidv4 generates unique values.
+	 *
+	 * This test verifies that the guidv4 function generates unique values.
+	 * It generates 100 unique guids and checks that they are all unique.
+	 *
+	 * @return void
+	 */
 	public function testGuidv4GeneratesUniqueValues() {
 		$event_ids = array();
 		for ( $i = 0; $i < 100; $i++ ) {
