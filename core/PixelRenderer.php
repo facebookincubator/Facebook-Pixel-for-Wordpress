b709268e7eb7d64bce4e4a18ad9256c6e5014ce3
Nikola Pavlovic <nikola@stuntcoders.com>
2024-10-28 09:11:41 +0100

    Update code style for PixelRenderer

--- core/PixelRenderer.php
+++ core/PixelRenderer.php
@@ -1,39 +1,61 @@
-<?php
-/*
- * Copyright (C) 2017-present, Meta, Inc.
+<?php //phpcs:ignore WordPress.Files.FileName.NotHyphenatedLowercase WordPress.Files.FileName.InvalidClassFileName
+/**
+ * Facebook Pixel Plugin PixelRenderer class.
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; version 2 of the License.
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * This file contains the main logic for PixelRenderer.
+ *
+ * @package FacebookPixelPlugin
  */
 
 /**
- * @package FacebookPixelPlugin
+ * Define PixelRenderer class.
+ *
+ * @return void
  */
+
+/*
+* Copyright (C) 2017-present, Meta, Inc.
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; version 2 of the License.
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*/
 
 namespace FacebookPixelPlugin\Core;
 
 use ReflectionClass;
 use FacebookAds\Object\ServerSide\CustomData;
 
-defined( 'ABSPATH' ) or die( 'Direct access not allowed' );
+defined( 'ABSPATH' ) || die( 'Direct access not allowed' );
 
+/**
+ * Class PixelRenderer
+ */
 class PixelRenderer {
 	const EVENT_ID                = 'eventID';
 	const TRACK                   = 'track';
 	const TRACK_CUSTOM            = 'trackCustom';
 	const FB_INTEGRATION_TRACKING = 'fb_integration_tracking';
 	const SCRIPT_TAG              = "<script type='text/javascript'>%s</script>";
-	const FBQ_EVENT_CODE          = "
-    fbq('%s', '%s', %s, %s);
-  ";
-	const FBQ_AGENT_CODE          = "
-  fbq('set', 'agent', '%s', '%s');";
+	const FBQ_EVENT_CODE          = "fbq('%s', '%s', %s, %s);";
+	const FBQ_AGENT_CODE          = "fbq('set', 'agent', '%s', '%s');";
 
+	/**
+	 * Render the pixel events
+	 *
+	 * @param array $events The array of events, each event is an array with the following keys:
+	 *                      - event_name: the name of the event
+	 *                      - event_id: the id of the event (optional)
+	 *                      - custom_data: the custom data for the event (optional).
+	 * @param bool  $fb_integration_tracking Whether to track the event as a Facebook integration.
+	 * @param bool  $script_tag Whether to wrap the generated code with a script tag.
+	 *
+	 * @return string The rendered pixel events
+	 */
 	public static function render(
 		$events,
 		$fb_integration_tracking,
@@ -42,20 +64,27 @@ class PixelRenderer {
 		if ( empty( $events ) ) {
 			return '';
 		}
-		// The first line of the injected script will set the agent
-		// for all the events passed in $events
 		$code = sprintf(
 			self::FBQ_AGENT_CODE,
 			FacebookWordpressOptions::get_agent_string(),
 			FacebookWordpressOptions::get_pixel_id()
 		);
 		foreach ( $events as $event ) {
-			$code .= self::getPixelTrackCode( $event, $fb_integration_tracking );
+			$code .= self::get_pixel_track_code( $event, $fb_integration_tracking );
 		}
 		return $script_tag ? sprintf( self::SCRIPT_TAG, $code ) : $code;
 	}
 
-	private static function getPixelTrackCode( $event, $fb_integration_tracking ) {
+
+	/**
+	 * Generate the Facebook Pixel track code for an event
+	 *
+	 * @param \FacebookPixelPlugin\Core\Event $event The event to generate the track code for.
+	 * @param bool                            $fb_integration_tracking Whether to track the event as a Facebook integration.
+	 *
+	 * @return string The generated track code
+	 */
+	private static function get_pixel_track_code( $event, $fb_integration_tracking ) {
 		$event_data[ self::EVENT_ID ] = $event->getEventId();
 
 		$custom_data = $event->getCustomData() !== null ?
@@ -73,8 +102,8 @@ class PixelRenderer {
 			$class->getConstant( strtoupper( $event->getEventName() ) ) !== false
 			? self::TRACK : self::TRACK_CUSTOM,
 			$event->getEventName(),
-			json_encode( $normalized_custom_data, JSON_PRETTY_PRINT ),
-			json_encode( $event_data, JSON_PRETTY_PRINT )
+			wp_json_encode( $normalized_custom_data, JSON_PRETTY_PRINT ),
+			wp_json_encode( $event_data, JSON_PRETTY_PRINT )
 		);
 	}
 }
