72e9fd9b5181d9e29283f5ae3917fb03c925acb4
Luka Garic <luka@stuntcoders.com>
2024-10-28 14:00:54 +0100

    Fix escaping for class-facebookwordpresswpforms

--- integration/class-facebookwordpresswpforms.php
+++ integration/class-facebookwordpresswpforms.php
@@ -123,7 +123,7 @@ class FacebookWordpressWPForms extends FacebookWordpressIntegrationBase {
 %s
 <!-- End Meta Pixel Event Code -->
       ',
-			$pixel_code //phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+			wp_kses_post( $pixel_code )
 		);
 	}
 
