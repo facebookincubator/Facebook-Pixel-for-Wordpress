728084bb8a84d2f553b05fcb02bc7c42da632261
Nikola Pavlovic <nikola@stuntcoders.com>
2024-11-04 09:58:43 +0100

    Add missing helper functions for tests

--- __tests__/WPHelpers.php
+++ __tests__/WPHelpers.php
@@ -29,3 +29,91 @@ if (!function_exists('sanitize_email')) {
         return filter_var(trim($email), FILTER_SANITIZE_EMAIL);
     }
 }
+
+if (!function_exists('plugins_url')) {
+    function plugins_url($path = '', $plugin = '') {
+        $base_url = 'http://' . $_SERVER['HTTP_HOST'] . '/wp-content/plugins/';
+
+        if (!empty($plugin)) {
+            $plugin_dir = dirname(plugin_basename($plugin));
+            if ('.' !== $plugin_dir) {
+                $base_url .= trailingslashit($plugin_dir);
+            }
+        }
+
+        return $base_url . ltrim($path, '/');
+    }
+}
+
+if (!function_exists('trailingslashit')) {
+    function trailingslashit($string) {
+        return rtrim($string, '/') . '/';
+    }
+}
+
+if (!function_exists('plugin_basename')) {
+    function plugin_basename($file) {
+        return str_replace(WP_PLUGIN_DIR . '/', '', $file);
+    }
+}
+
+if (!defined('WP_PLUGIN_DIR')) {
+    define('WP_PLUGIN_DIR', $_SERVER['DOCUMENT_ROOT'] . '/wp-content/plugins');
+}
+
+
+if (!function_exists('wp_localize_script')) {
+    function wp_localize_script($handle, $object_name, $l10n) {
+        if (!is_string($object_name) || empty($object_name)) {
+            return false;
+        }
+
+        $data = [];
+        foreach ($l10n as $key => $value) {
+            $data[] = sprintf('%s: %s', json_encode($key), json_encode($value));
+        }
+
+        $script = sprintf(
+            '<script type="text/javascript">var %s = {%s};</script>',
+            $object_name,
+            implode(',', $data)
+        );
+
+        echo $script;
+        return true;
+    }
+}
+
+
+if (!function_exists('wp_enqueue_script')) {
+    function wp_enqueue_script($handle, $src = '', $deps = array(), $ver = false, $in_footer = false) {
+        static $enqueued_scripts = [];
+
+        // Avoid duplicate enqueuing
+        if (isset($enqueued_scripts[$handle])) {
+            return;
+        }
+
+        $version = $ver ? '?ver=' . $ver : '';
+        $script_url = $src . $version;
+
+        if ($in_footer) {
+            add_to_footer_queue("<script src=\"$script_url\" type=\"text/javascript\"></script>");
+        } else {
+            echo "<script src=\"$script_url\" type=\"text/javascript\"></script>\n";
+        }
+
+        $enqueued_scripts[$handle] = $script_url;
+    }
+
+    function add_to_footer_queue($script_tag) {
+        static $footer_scripts = [];
+        $footer_scripts[] = $script_tag;
+
+        register_shutdown_function(function() use (&$footer_scripts) {
+            foreach ($footer_scripts as $script) {
+                echo $script . "\n";
+            }
+        });
+    }
+}
