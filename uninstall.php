57285bfd32d2243dcf61ae573410d0c310bb888d
Marko Ranđelović <marko@stuntcoders.com>
2025-01-21 10:11:36 +0100

    Remove plugin settings matching old DB key on uninstall

--- uninstall.php
+++ uninstall.php
@@ -29,4 +29,6 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 }
 
 delete_option( FacebookPluginConfig::SETTINGS_KEY );
+delete_option( FacebookPluginConfig::OLD_SETTINGS_KEY );
+
 delete_user_meta( get_current_user_id(), FacebookPluginConfig::ADMIN_IGNORE_PIXEL_ID_NOTICE );
