4bec7cce85b9dd712dac74e64546cf7d8fbf2448
Vahid Kaykhaei <vahidkay@meta.com>
2025-05-27 10:05:39 +0200

    Minor indentation fix

--- core/class-eventidgenerator.php
+++ core/class-eventidgenerator.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Facebook Pixel Plugin EventIdGenerator class.
  *
@@ -37,14 +38,14 @@ final class EventIdGenerator {
      * @return string A 36 character string containing dashes.
      */
     public static function guidv4() {
-        $data = openssl_random_pseudo_bytes( 16 );
+        $data = openssl_random_pseudo_bytes(16);
 
-        $data[6] = chr( ord( $data[6] ) & 0x0f | 0x40 );
-        $data[8] = chr( ord( $data[8] ) & 0x3f | 0x80 );
+        $data[6] = chr(ord($data[6]) & 0x0f | 0x40);
+        $data[8] = chr(ord($data[8]) & 0x3f | 0x80);
 
-    return vsprintf(
-        '%s%s-%s-%s-%s-%s%s%s',
-        str_split( bin2hex( $data ), 4 )
-    );
+        return vsprintf(
+            '%s%s-%s-%s-%s-%s%s%s',
+            str_split(bin2hex($data), 4)
+        );
     }
 }
