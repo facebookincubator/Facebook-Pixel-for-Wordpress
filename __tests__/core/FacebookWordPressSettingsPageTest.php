11b11c600e340b4158e85ad74bfa82c014c1cfe4
Vahid Kaykhaei <vahidkay@meta.com>
2026-05-05 10:13:01 +0200

    Updated the banner to ensure it communicates the right message

--- __tests__/core/FacebookWordPressSettingsPageTest.php
+++ __tests__/core/FacebookWordPressSettingsPageTest.php
@@ -62,7 +62,7 @@ final class FacebookWordPressSettingsPageTest extends FacebookWordpressTestBase 
     $message = $settings_page->get_wpcom_update_notice_message();
 
     $this->assertStringContainsString(
-      'After April 30th, 2026',
+      'Meta Pixel for WordPress is expected to be removed',
       $message
     );
     $this->assertStringContainsString(
