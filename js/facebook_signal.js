1b000221035d38a92560910a0942f1cafdb6a282
Vahid Kaykhaei <vahidkay@meta.com>
2026-09-02 15:48:09 +0100

    Reference LICENSE file in JS and shell headers
    Replaces the inline GPLv2 recital in the JavaScript and shell headers
    with a short reference to the LICENSE file, modelled on the existing
    header in css/admin.css.
    
    PHP files keep the full GPLv2 block, matching the convention used
    throughout the rest of the codebase.

--- js/facebook_signal.js
+++ js/facebook_signal.js
@@ -1,13 +1,8 @@
 /**
  * Copyright (C) 2017-present, Meta, Inc.
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; version 2 of the License.
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
  */
 
 window.FacebookSignal = window.FacebookSignal || {
